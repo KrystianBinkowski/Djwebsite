4a40c8087f76ed70a4af19baae9d63f8ec2beea3
Czortez <czortez@MacBook-Pro-Czortez.local>
2018-05-12 14:29:55 +0200

    Security

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,19 +1,31 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class AdminController extends Controller
 {
     /**
      * @Route("/admin", name="admin")
      */
-    public function index()
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
     {
-        return $this->render('admin/index.html.twig', [
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('admin/index.html.twig', array(
             'controller_name' => 'AdminController',
-        ]);
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
     }
+
+
 }
+
+
