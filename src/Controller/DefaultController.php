a2a9f1e458ff2ee9d270eca9d494cd84c52d122a
Czortez <krystian.binkowski1984@gmail.com>
2018-05-26 12:00:25 +0200

    database

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,8 +12,21 @@ class DefaultController extends Controller
      */
     public function index()
     {
+        $DjSet = $this->getDoctrine()
+        ->getRepository(Product::class)
+        ->findAll($id);
+
+    if (!$product) {
+        throw $this->createNotFoundException(
+            'No product found for id '.$id
+        );
+    }
+
+    return new Response('Check out this great product: '.$DjSet->getName());
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
+   
 }
